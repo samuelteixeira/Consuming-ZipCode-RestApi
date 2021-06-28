465e2f57488d39af4d572c2420ec1c90c94c3dbd
Samuel Teixeira <samuelteixeira.dev@gmail.com>
2021-06-28 15:44:13 -0300

    console project creation

---
+++ Consuming-Api-Rest-Cep/Program.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace Consuming_Api_Rest_Cep
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+                Console.WriteLine("Informe o seu cep");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+               
+                Console.WriteLine($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}\nCidade:{address.Localidade}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de CEP " + e.Message);
+            }
+        }
+    }
+}
